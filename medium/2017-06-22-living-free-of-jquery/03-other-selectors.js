ba24e77f57df8d637381b188bceace23fd464ad6
João Milton <jmlavoier@hotmail.com>
2017-06-27 16:59:56 -0300

    Add: other selectors

---
+++ medium/2017-06-22-living-free-of-jquery/03-other-selectors.js
@@ -0,0 +1,3 @@
+console.log(document.getElementById('cont')); // <div id="cont" class="content">...</div>
+console.log(document.getElementsByClassName('content')); // [div#cont.content]
+console.log(document.getElementsByTagName('div')); // [div, div, div#cont.content]

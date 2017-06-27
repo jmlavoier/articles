994b03b87083c0086dfb80035d8cd552c6ea9832
João Milton <jmlavoier@hotmail.com>
2017-06-27 16:57:55 -0300

    Refactor: selectors

--- medium/2017-06-22-living-free-of-jquery/02-queryselector.js
+++ medium/2017-06-22-living-free-of-jquery/02-queryselector.js
@@ -1,2 +1,19 @@
-let content = document.querySelector('.content');
-let p = content.querySelector('p'); // <p>Paragrafo 1</p>
+const content = document.querySelector('.content');
+let p = document.querySelector('p');
+let pList = document.querySelectorAll('p');
+
+console.log(p); // <p class="p1">Paragrafo 1</p>
+
+p = document.querySelector('.content > p.p3');
+
+console.log(p); // <p class="p3">...</p>
+console.log(pList); // [p.p1, p.p2, p.p3]
+
+pList = document.querySelectorAll('.content > p.p3');
+
+console.log(pList); // [p.3]
+console.log(content); // <div id="cont" class="content">...</div>
+
+content = content.querySelectorAll('.p3');
+
+console.log(pList); // <p class="p3">...</p>

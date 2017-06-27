60ae9171764f3ce5cb4579254eb95aad3e000a9e
João Milton <jmlavoier@hotmail.com>
2017-06-27 17:01:44 -0300

    Add: parent

---
+++ medium/2017-06-22-living-free-of-jquery/04-parent.js
@@ -0,0 +1,4 @@
+let p = document.querySelector('.content > p');
+
+console.log(document.parentNode); // <div id="cont" class="content">...</div>
+console.log(document.parentElement);  // <div id="cont" class="content">...</div>
